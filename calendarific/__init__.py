91448fcecff680861d76a419599a31a2a7d4a452
MATTHIAS BRENNINKMEIJER <matthias.brenninkmeijer@mango.com>
2021-03-05 18:32:39 +0100

    Added country parameter

--- calendarific/__init__.py
+++ calendarific/__init__.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+
 class v2:
     api_key = None
 
@@ -13,8 +14,8 @@ class v2:
         if 'api_key' not in parameters:
             parameters['api_key'] = self.api_key
 
-        response = requests.get(url, params=parameters);
-        data     = json.loads(response.text)
+        response = requests.get(url, params=parameters)
+        data = json.loads(response.text)
 
         if response.status_code != 200:
             if 'error' not in data:
@@ -22,3 +23,17 @@ class v2:
 
         return data
 
+    def countries(self, parameters):
+        url = 'https://calendarific.com/api/v2/countries?'
+
+        if 'api_key' not in parameters:
+            parameters['api_key'] = self.api_key
+
+        response = requests.get(url, params=parameters)
+        data = json.loads(response.text)
+
+        if response.status_code != 200:
+            if 'error' not in data:
+                data['error'] = 'Unknown error.'
+
+        return data
